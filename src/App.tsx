37bfc914b71e92e22d87d6c27019dfad88e73634
Janne Arkko <ab3817@student.jamk.fi>
2024-06-10 11:27:03 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.MItä persettä!? TOimiiko tämä??
+          Edit <code>src/App.tsx</code> and save to reload.SIis vieläkin toimii?KO?
         </p>
         <a
           className="App-link"
