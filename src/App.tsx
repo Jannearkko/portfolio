9a939857cfb88e30527294b011aab332463a53e2
Janne Arkko <ab3817@student.jamk.fi>
2024-06-10 10:53:32 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. Jeejee.
+          Edit <code>src/App.tsx</code> and save to reload. Jeejee.asd
         </p>
         <a
           className="App-link"
